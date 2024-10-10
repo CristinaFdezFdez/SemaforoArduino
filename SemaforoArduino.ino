3e414b595c26c26b77a3ac9c9e6ea8fd3c5c81e6
Cristina Fernández Fernández <cristina.98fdz@gmail.com>
2024-10-10 12:09:54 +0200

    first commit

---
+++ SemaforoArduino.ino
@@ -0,0 +1,28 @@
+int ledRoja = 12;
+int ledAzul = 10;
+int ledAmarilla = 8;
+
+float tiempoEncendido = 2000; 
+
+void setup() {
+  pinMode(ledRoja, OUTPUT);  
+  pinMode(ledAzul, OUTPUT); 
+  pinMode(ledAmarilla, OUTPUT); 
+}
+
+void loop() {
+
+  digitalWrite(ledRoja, HIGH);
+  delay(tiempoEncendido);
+  digitalWrite(ledRoja, LOW);
+  
+  digitalWrite(ledAzul, HIGH);
+  delay(tiempoEncendido);
+  digitalWrite(ledAzul, LOW);
+  
+  digitalWrite(ledAmarilla, HIGH);
+  delay(tiempoEncendido);
+  digitalWrite(ledAmarilla, LOW);
+  
+  tiempoEncendido = (tiempoEncendido > 100) ? tiempoEncendido - 100 : 100;  
+}
